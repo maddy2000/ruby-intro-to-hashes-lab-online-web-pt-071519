f082989f36f3a5ef56750b8836b1d2f89d39618a
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-15 21:21:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,20 @@
 def new_hash
+  empty = {}
+  end
 
-end
-
-def actor
-
+def actor 
+  {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {:railroads => {}}
+	
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+		monopoly = {:railroads => {}}
+              :
 
 end
 
