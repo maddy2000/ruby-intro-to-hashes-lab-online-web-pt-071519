b0622378cea80d69b9d56934e2fd757958bd4b49
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-16 00:32:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,9 @@ def monopoly_with_second_tier
 		    :names => {}
 		     # :reading_railroad => { 
 		       # "mortgage_value" => "$100"
-		      },
-		      :pennsylvania_railroad => {
-		        "mortgage_value" => "$200"
+	#	      },
+	#	      :pennsylvania_railroad => {
+	#	        "mortgage_value" => "$200"
 		      },
 		      :b_and_o_railroads => {
 		        "mortgage_value" => "$400"
