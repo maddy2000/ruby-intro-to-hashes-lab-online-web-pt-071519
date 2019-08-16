d9c4d1b630da8017aca7199a1e73e90bcabdcf20
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-16 00:59:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,9 @@ def monopoly_with_third_tier
 	        :two_pieces_owned => 50,
 	        :three_pieces_owned => 100,
 	        :four_pieces_owned => 200
-	        :reading_railroad => {
-	          "mortgage_value" => "$100"
-	        }
+	        :reading_railroad => {}
+	          
+	       
 	      }
 	      
 	    }
